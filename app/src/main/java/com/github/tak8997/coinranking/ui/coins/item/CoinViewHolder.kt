0ccaa9d6f75275d92e62c562a9f50ee3777224c2
Tak <tak8997@gmail.com>
2019-09-10 16:33:08 +0900

    좋아요 상태 개선

--- app/src/main/java/com/github/tak8997/coinranking/ui/coins/item/CoinViewHolder.kt
+++ app/src/main/java/com/github/tak8997/coinranking/ui/coins/item/CoinViewHolder.kt
@@ -4,7 +4,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.github.tak8997.coinranking.data.model.Coin
 import com.github.tak8997.coinranking.databinding.ItemCoinBinding
 
-class CoinViewHolder(private val binding: ItemCoinBinding) : RecyclerView.ViewHolder(binding.root) {
+class CoinViewHolder(val binding: ItemCoinBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(coin: Coin?) {
         binding.item = coin

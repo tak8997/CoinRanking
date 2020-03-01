2a6abeada159e240aaf4a33720d620b7d0c04347
kyle lee <kyle.lee@balancehero.com>
2020-03-01 16:08:50 +0900

    add coroutine app config

--- app_coroutine/src/androidTest/java/com/tak8997/app_coroutine/ExampleInstrumentedTest.kt
+++ app_coroutine/src/androidTest/java/com/tak8997/app_coroutine/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.tak8997.app_coroutine.test", appContext.packageName)
+        assertEquals("com.tak8997.app_coroutine", appContext.packageName)
     }
 }
